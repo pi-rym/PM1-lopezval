2163e388a82692e171f9c01cdbb24af79c4374f4
lopezval <mario.lopez.v@gmail.com>
2024-03-13 15:20:59 -0500

    completada homework 2 creada clases Activity y Repository con sus constructores y metodos.

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,31 @@
 class Activity{
     constructor(id, title, description, imgUrl){
-        this.id= id;
-        this.title= title;
-        this.description= description;
-        this.imgUrl= imgUrl;
-
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.imgUrl = imgUrl;
     }
 }
 
 class Repository{
     constructor(){
         this.activities = [];
-        this.id= 0; 
+        this.id = 0;
     }
-
     getAllActivities(){
         return this.activities;
     }
-
     createActivity(title, description, imgUrl){
-        const id = this.id++;
+        const id = ++this.id;
         const activity = new Activity(id, title, description, imgUrl);
-        this.activities.push(activity);        
+        this.activities.push(activity)
     }
-
-    deleteActivity(eliminarId){
-        const id = eliminarId;
-        const filterActivities = this.activities.filter((activities) => activities.id !== this.id);
+    deleteActivity(id){
+        const filteredActivities = this.activities.filter((activities) => activities.id !== id);
+        this.activities = filteredActivities;        
     }
 }
+
+
+
+
